b07efa762c95c3e18185200c9dc103daddea8861
Umayer <kabir20304@gmail.com>
2025-06-08 01:10:27 +0600

    nothing

--- restaurant.py
+++ restaurant.py
@@ -10,6 +10,18 @@ class Restaurant:
         self.profit=0
         self.order=[]
 
-        
+    def add_employee(self,employee,employee_type)
+        if employee_type == 'chef':
+            self.chef=employee
+        elif employee_type == 'server':
+            self.server.append(employee)
+        elif employee_type == 'Manager':
+            self.manager=employee
+    
+    def receive_payment(self, order,amount,customer):
+        if amount >=order.bill:
+            self.revenue+=amount
+            
+
 
         
